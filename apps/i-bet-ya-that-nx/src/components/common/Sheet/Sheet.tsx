da5034e4f192da5f7e6e2689377046aad9f21c62
Ralph Juden <ralphjuden@gmail.com>
2024-07-04 11:50:14 +0200

    Add Sheet layout component

---
+++ apps/i-bet-ya-that-nx/src/components/common/Sheet/Sheet.tsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx';
+
+type Props = {
+  children: React.ReactNode;
+  fullWidth?: boolean;
+  className?: string;
+};
+
+export const Sheet = ({ children, fullWidth, className }: Props) => {
+  return (
+    <div
+      className={clsx(
+        'px-4 mx-auto',
+        fullWidth ? 'w-full' : 'max-w-7xl',
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
