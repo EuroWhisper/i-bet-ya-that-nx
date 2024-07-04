c730c29fbce81430bbd30b76725dcad9921a8063
Ralph Juden <ralphjuden@gmail.com>
2024-07-04 16:51:39 +0200

    Add additional information to home page

--- apps/i-bet-ya-that-nx/src/modules/home/HomeContent.tsx
+++ apps/i-bet-ya-that-nx/src/modules/home/HomeContent.tsx
@@ -34,6 +34,32 @@ const HomeContent = ({ existingPredictions, predictionSuggestion }: Props) => {
             <PredictionsList predictions={existingPredictions} />
           </PageSection>
         )}
+        <PageSection title="What is this?">
+          <p>
+            This is an application where you can make predictions and track how
+            often you are right or wrong. You can suggest a prediction and set a
+            date when it should be verified. You will receive an email with a
+            link to verify the prediction.
+          </p>
+        </PageSection>
+        <PageSection title="What else will be added?">
+          <p>
+            Soon, a point system will be implemented to keep track of your score
+            and a leaderboard will be added to see who is the best predictor.
+          </p>
+          <p>
+            Additionally, AI will be used to check the outcome of the
+            predictions where possible. Not all predictions can be verified by
+            AI, so some will have to be manually verified.
+          </p>
+          <p>
+            The plan right now is to lock the amount of points you get when you
+            successfully predict something to a lower amount if you had to
+            manually verify it. While more points will be awarded if the AI can
+            verify it. This is to help stop people from spamming predictions for
+            lots of points that cannot be verified.
+          </p>
+        </PageSection>
       </Sheet>
     </main>
   );
